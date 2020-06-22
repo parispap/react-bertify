fe81ac762206462b267faa79affa6550721b6c52
Paris Papadopoulos <parispap1995@gmail.com>
2020-06-22 11:42:57 +0300

    Updates

---
+++ precache-manifest.e535406efbc3d0b4e22fedab670e89bb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a61a87da28424f6d8480425e361448f",
+    "url": "/react-bertify/index.html"
+  },
+  {
+    "revision": "0d5805eceff4cf9bc737",
+    "url": "/react-bertify/static/css/main.0b5c4bfe.chunk.css"
+  },
+  {
+    "revision": "0b25be9dcb6b13857ae4",
+    "url": "/react-bertify/static/js/2.ae5daeae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-bertify/static/js/2.ae5daeae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d5805eceff4cf9bc737",
+    "url": "/react-bertify/static/js/main.c5058eaa.chunk.js"
+  },
+  {
+    "revision": "b5bd2aa725ed6cfdf7ee",
+    "url": "/react-bertify/static/js/runtime-main.1ae9d080.js"
+  },
+  {
+    "revision": "7f3ab5fd898db3d04e26f775ade20960",
+    "url": "/react-bertify/static/media/Cocoa_Fruit.7f3ab5fd.png"
+  },
+  {
+    "revision": "8ee80aa5944ccd0fd457c12e0ad6a96e",
+    "url": "/react-bertify/static/media/Statistics.8ee80aa5.webp"
+  },
+  {
+    "revision": "91f7effe7d29a902e2e0d8b11000b96d",
+    "url": "/react-bertify/static/media/left_cocoa_beans.91f7effe.png"
+  },
+  {
+    "revision": "15ab19054a9e7d880950c4ad0ca064a1",
+    "url": "/react-bertify/static/media/protein_powder.15ab1905.png"
+  },
+  {
+    "revision": "96c7748287f1bf80e07d52e39fcb1d55",
+    "url": "/react-bertify/static/media/right_cocoa_beans.96c77482.png"
+  }
+]);
